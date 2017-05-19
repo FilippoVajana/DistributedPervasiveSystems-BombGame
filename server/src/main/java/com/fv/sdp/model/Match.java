29c509a4d8177cc9baf39d4bcbee7e4e56ee1a76
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2017-05-19 09:54:41 +0200

    aggiunta lista giocatori all'interno di una partita

--- server/src/main/java/com/fv/sdp/model/Match.java
+++ server/src/main/java/com/fv/sdp/model/Match.java
@@ -1,5 +1,7 @@
 package com.fv.sdp.model;
 
+import com.fv.sdp.util.ConcurrentList;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -11,6 +13,7 @@ public class Match
     private String id;
     private int edgeLength;
     private int victoryPoints;
+    private ConcurrentList<Player> players;
 
     public Match() {
     }
@@ -19,6 +22,14 @@ public class Match
         this.id = id;
         this.edgeLength = edgeLength;
         this.victoryPoints = victoryPoints;
+        this.players = new ConcurrentList<>();
+    }
+
+    public Match(String id, int edgeLength, int victoryPoints, ConcurrentList<Player> players) {
+        this.id = id;
+        this.edgeLength = edgeLength;
+        this.victoryPoints = victoryPoints;
+        this.players = players;
     }
 
     public String getId() {
@@ -43,5 +54,26 @@ public class Match
 
     public void setVictoryPoints(int victoryPoints) {
         this.victoryPoints = victoryPoints;
+    }
+
+    public ConcurrentList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ConcurrentList<Player> players) {
+        this.players = players;
+    }
+
+    @Override
+    //equality over id
+    public boolean equals(Object match)
+    {
+        if (match == null)
+        return false;
+
+        Match m = (Match) match;
+        if (this.getId() == m.getId())
+            return true;
+        return false;
     }
 }
