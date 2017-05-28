6c85f70209a24ad7b9cc507a1b49e145d82a79fc
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2017-05-28 12:24:08 +0200

    riorganizzata classe RESTConnector lato client

--- client/src/main/java/main/java/rest/RESTConnector.java
+++ client/src/main/java/main/java/rest/RESTConnector.java
@@ -1,39 +1,65 @@
 package main.java.rest;
 
+import com.fv.sdp.model.Match;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by filip on 5/27/2017.
  */
 public class RESTConnector
 {
-    final String serverBaseUrl = "http://localhost:8080/server_war_exploded";
+    private Client restClient = null;
+    private WebTarget restBaseUrl = null;
+    private Map<String, String> restEndpointsIndex = null;
+
+    public RESTConnector(String serviceBaseUrl, HashMap<String, String> serviceEndpoints)
+    {
+        //client
+        restClient = ClientBuilder.newClient();
+        //base url
+        restBaseUrl = restClient.target(serviceBaseUrl);
+        //endpoints
+        restEndpointsIndex = serviceEndpoints;
+    }
+
     public boolean requestSetTestModel()
     {
-        //rest client
-        Client client = ClientBuilder.newClient();
-
-        //match web target
-        WebTarget matchBaseTarget = client.target(serverBaseUrl).path("match");
+        WebTarget matchTarget = restBaseUrl.path(restEndpointsIndex.get("Match"));
         //test model web target
-        WebTarget testModelTarget = matchBaseTarget.path("setTest");
+        WebTarget testModelTarget = matchTarget.path("setTest");
 
         //invocation
         Invocation.Builder invocation = testModelTarget.request();
 
         //make request
-        Response response =testModelTarget.request(MediaType.TEXT_PLAIN).get();
+        Response response = invocation.get();
         return true;
     }
 
+    public ArrayList<Match> requestMatchesList()
+    {
+        return null;
+    }
 
     public static void main(String[] args)
     {
-        new RESTConnector().requestSetTestModel();
+        //base url
+        String serverBaseUrl = "http://localhost:8080/server_war_exploded";
+        //service endpoints
+        HashMap<String, String> serverEndpoints = new HashMap<>();
+        serverEndpoints.put("Match", "match");
+
+        RESTConnector rest = new RESTConnector(serverBaseUrl, serverEndpoints );
+
+        //set test model
+        rest.requestSetTestModel();
     }
 }
