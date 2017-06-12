84b23a4959b96f38942a4c03e9534502d840f7ee
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2017-06-12 23:04:12 +0200

    implementato gestore di messaggi ACK

---
+++ client/src/main/java/main/java/ring/AckHandler.java
@@ -0,0 +1,46 @@
+package main.java.ring;
+
+import main.java.socket.RingMessage;
+import main.java.util.ConcurrentObservableQueue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by filip on 6/11/2017.
+ */
+public class AckHandler implements IMessageHandler
+{
+    private Map<String, Integer> queueMap; //retain a map of pending ack to be received (id:count)
+    private Object tokenLock;
+
+    public AckHandler(Object tokenLock)
+    {
+        //init map
+        queueMap = new HashMap<>();
+        this.tokenLock = tokenLock;
+    }
+
+    public void addPendingAck(String id, int num)
+    {
+        //init queue
+        queueMap.put(id, num);
+    }
+
+    //TODO implement logic
+    @Override
+    public synchronized void handle(RingMessage message)
+    {
+        //find message ack queue
+        Integer ackCount = queueMap.get(message.getId());
+        //decrement ack count
+        ackCount--;
+
+        if (ackCount == 0)
+        {
+            queueMap.remove(message.getId());
+            //notify Token Managar
+            tokenLock.notify();
+        }
+    }
+}
