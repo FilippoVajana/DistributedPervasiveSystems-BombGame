f55f8f30e5ca2b5fbdd54d7bd649c9a9ed3de4d2
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2017-07-05 19:18:20 +0200

    aggiunto a RingMessage campo token
    la variabile di tipo booleano viene usata da SocketConnector per determinare la necessità o meno di attendere il possesso del token per l'invio del messaggio

--- client/src/main/java/com/fv/sdp/socket/RingMessage.java
+++ client/src/main/java/com/fv/sdp/socket/RingMessage.java
@@ -6,9 +6,10 @@ package com.fv.sdp.socket;
 public class RingMessage
 {
     private MessageType type;
-    private String sourceAddress; //set on socket receive
+    private String sourceAddress; //set on socket sent
     private String id;
     private String content;
+    private boolean needToken = true;
 
     public RingMessage()
     {    }
@@ -16,12 +17,20 @@ public class RingMessage
     {
         this.type = type;
         this.id = id;
+
+        //type check
+        if (type == MessageType.ACK)
+            needToken = false;
     }
     public RingMessage(MessageType type, String id, String content)
     {
         this.type = type;
         this.id = id;
         this.content = content;
+
+        //type check
+        if (type == MessageType.ACK)
+            needToken = false;
     }
     public RingMessage(MessageType type, String source, String id, String content)
     {
@@ -29,38 +38,36 @@ public class RingMessage
         this.sourceAddress = source;
         this.id = id;
         this.content = content;
+
+        //type check
+        if (type == MessageType.ACK)
+            needToken = false;
     }
 
     public MessageType getType() {
         return type;
     }
 
-    public void setType(MessageType type) {
-        this.type = type;
-    }
-
     public String getSourceAddress() {
         return sourceAddress;
     }
-
     public void setSourceAddress(String sourceAddress) {
         this.sourceAddress = sourceAddress;
     }
 
-    public String getId() {
+    public String getId()
+    {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getContent() {
+    public String getContent()
+    {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setNeedToken(boolean needToken)
+    {
+        this.needToken = needToken;
     }
 
     @Override
